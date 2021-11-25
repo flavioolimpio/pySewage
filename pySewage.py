0b5a90f109ed89fba121b5b525cfcc32b3e5848d
flavioolimpio <34543135+flavioolimpio@users.noreply.github.com>
2021-11-25 11:40:27 -0300

    Update pySewage.py

--- pySewage.py
+++ pySewage.py
@@ -5,6 +5,7 @@ from texts import Texts
 import math
 import random
 import base64
+from pyxlsb import open_workbook as open_xlsb
 from io import BytesIO
 import os
 from PIL import Image
@@ -21,8 +22,8 @@ nav = st.sidebar.radio('Ir para:', ['Home', 'Simulation'])
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='csvwriter')
-    df.to_csv(writer, sheet_name='Sheet1')
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
     writer.save()
     processed_data = output.getvalue()
     return processed_data
@@ -34,7 +35,7 @@ def get_table_download_link(df):
     """
     val = to_excel(df)
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.csv">Download csv file</a>' # decode b'abc' => abc
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
 
 if nav == 'Home':
     gettext = Texts()
