08a165633c85090df05ac6a342bd4b63f8c80c8b
flavioolimpio <34543135+flavioolimpio@users.noreply.github.com>
2021-11-25 11:08:24 -0300

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,28 @@
+class Texts:
+    def __init__(self):
+        pass
+
+    def text1(self):
+        TEXT1 = """
+        This application consists of the automatic prediction of reaction rate constant of the radical-based oxidation process of 
+        aqueous and atmospheric organic contaminants based on Machine Learning models using molecular fingerprints. 
+        It is only necessary to inform the SMILES or CAS Number format of a specific molecule. 
+        The models present in this work were built using scikit-learn and XGBoost packages. 
+        The predict values can be confronted with experimental values available in our work or kinetics.nist.gov/solution
+        
+        """
+
+        return TEXT1
+
+    def text3(self):
+        TEXT3 = """
+        ## About
+        pySEAGE was designed to facilitate the automatic researchers to prediction and analyze of viral RNA concetration of SARS-CoV-2
+        using Monte Carlo Simulation. The tools are designed to be quick and simple users with few clicks
+        can make the prediction. The tools are rigorous enough to have applications in academic research
+
+        ## Supporters
+        """
+
+        return TEXT3
+    
