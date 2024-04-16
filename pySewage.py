af87a146162f2bd6cfcee2009cb615098957578c
flavioolimpio <34543135+flavioolimpio@users.noreply.github.com>
2024-04-16 11:40:36 -0300

    new pysewage
    new

--- pySewage.py
+++ pySewage.py
@@ -1,29 +1,201 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from texts import Texts
 import math
 import random
 from PIL import Image
 from streamlit_option_menu import option_menu
+from texts import Texts
+
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+def parameter_inputs(selected_virus=None):
+    col1, col2, col3 = st.columns(3)
+    
+    if selected_virus == 'DENV':
+        with col1:
+            min_alpha = st.text_input('Minimum alpha', value='2.1')
+        with col2:
+            max_alpha = st.text_input('Maximum alpha', value='3.8')
+        with col3:
+            alpha_description = st.empty()  # Placeholder for alpha description
+        
+        col4, col5, col6 = st.columns(3)
+    
+        with col4:
+            media_beta = st.text_input('Average beta', value='3.1')
+        with col5:
+            dp_beta = st.text_input('Standard deviation of beta', value='0.15')
+        with col6:
+            beta_description = st.empty()  # Placeholder for beta description
+        
+        col7, col8, col9 = st.columns(3)
+        
+        with col7:
+            min_gamma = st.text_input('Minimum gamma', value='0.25')
+        with col8:
+            max_gamma = st.text_input('Maximum gamma', value='0.80')
+        with col9:
+            gamma_description = st.empty()  # Placeholder for gamma description
+
+    elif selected_virus == 'ZIKV':
+        with col1:
+            min_alpha = st.text_input('Minimum alpha', value='5.85')
+        with col2:
+            max_alpha = st.text_input('Maximum alpha', value='8.34')
+        with col3:
+            alpha_description = st.empty()  # Placeholder for alpha description
+
+        col4, col5, col6 = st.columns(3)
+    
+        with col4:
+            media_beta = st.text_input('Average beta', value='3.1')
+        with col5:
+            dp_beta = st.text_input('Standard deviation of beta', value='0.15')
+        with col6:
+            beta_description = st.empty()  # Placeholder for beta description
+        
+        col7, col8, col9 = st.columns(3)
+        
+        with col7:
+            min_gamma = st.text_input('Minimum gamma', value='0.25')
+        with col8:
+            max_gamma = st.text_input('Maximum gamma', value='0.80')
+        with col9:
+            gamma_description = st.empty()  # Placeholder for gamma description
+
+    elif selected_virus == 'CHIKV':
+        with col1:
+            min_alpha = st.text_input('Minimum alpha', value='2.84')
+        with col2:
+            max_alpha = st.text_input('Maximum alpha', value='5.83')
+        with col3:
+            alpha_description = st.empty()  # Placeholder for alpha description
+
+        col4, col5, col6 = st.columns(3)
+    
+        with col4:
+            media_beta = st.text_input('Average beta', value='3.1')
+        with col5:
+            dp_beta = st.text_input('Standard deviation of beta', value='0.15')
+        with col6:
+            beta_description = st.empty()  # Placeholder for beta description
+        
+        col7, col8, col9 = st.columns(3)
+        
+        with col7:
+            min_gamma = st.text_input('Minimum gamma', value='0.25')
+        with col8:
+            max_gamma = st.text_input('Maximum gamma', value='0.80')
+        with col9:
+            gamma_description = st.empty()  # Placeholder for gamma description
+
+    else:
+        with col1:
+            min_alpha = st.text_input('Minimum alpha', value='6.3E5')
+        with col2:
+            max_alpha = st.text_input('Maximum alpha', value='1.3E8')
+        with col3:
+            alpha_description = st.empty()  # Placeholder for alpha description
+        
+        col4, col5, col6 = st.columns(3)
+    
+        with col4:
+            media_beta = st.text_input('Average beta', value='149')
+        with col5:
+            dp_beta = st.text_input('Standard deviation of beta', value='95')
+        with col6:
+            beta_description = st.empty()  # Placeholder for beta description
+        
+        col7, col8, col9 = st.columns(3)
+        
+        with col7:
+            min_gamma = st.text_input('Minimum gamma', value='0.25')
+        with col8:
+            max_gamma = st.text_input('Maximum gamma', value='0.80')
+        with col9:
+            gamma_description = st.empty()  # Placeholder for gamma description
+    
+    return min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma
+
+def generate_parameters_auto(selected_virus=None):
+    if selected_virus == 'DENV':
+        mc_rf_abc = [random.uniform(2.1, 3.8) for _ in range(50000)]
+        mc_f = [random.normalvariate(3.1, 0.15) for _ in range(50000)]
+        mc_e = [random.betavariate(0.25, 0.80) for _ in range(50000)]
+    elif selected_virus == 'ZIKV':
+        mc_rf_abc = [random.uniform(5.85, 8.34) for _ in range(50000)]
+        mc_f = [random.normalvariate(3.1, 0.15) for _ in range(50000)]
+        mc_e = [random.betavariate(0.25, 0.80) for _ in range(50000)]
+    elif selected_virus == 'CHIKV':
+        mc_rf_abc = [random.uniform(2.84, 5.83) for _ in range(50000)]
+        mc_f = [random.normalvariate(3.1, 0.15) for _ in range(50000)]
+        mc_e = [random.betavariate(0.25, 0.80) for _ in range(50000)]
+    else:
+        mc_rf_abc = [random.uniform(np.log10(6.3E5), np.log10(1.3E8)) for _ in range(50000)]
+        mc_f = [random.normalvariate(149, 95) for _ in range(50000)]
+        mc_e = [random.uniform(0.30, 0.90) for _ in range(50000)]
+
+    RF = np.mean(mc_rf_abc)
+    F = np.mean(mc_f)
+    E = np.mean(mc_e)
+
+    return RF, F, E
+
+def generate_parameters_manual(min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma, selected_virus=None):
+    if selected_virus == 'DENV':
+        mc_rf_alpha = [random.uniform(float(min_alpha), float(max_alpha)) for _ in range(50000)]
+        mc_f = [random.normalvariate(float(media_beta), float(dp_beta))  for _ in range(50000)]
+        mc_e = [random.betavariate(float(min_gamma), float(max_gamma)) for _ in range(50000)]
+
+    elif selected_virus == 'ZIKV':
+        mc_rf_alpha = [random.uniform(float(min_alpha), float(max_alpha)) for _ in range(50000)]
+        mc_f = [random.normalvariate(float(media_beta), float(dp_beta))  for _ in range(50000)]
+        mc_e = [random.betavariate(float(min_gamma), float(max_gamma)) for _ in range(50000)]
+    
+    elif selected_virus == 'CHIKV':
+        mc_rf_alpha = [random.uniform(float(min_alpha), float(max_alpha)) for _ in range(50000)]
+        mc_f = [random.normalvariate(float(media_beta), float(dp_beta))  for _ in range(50000)]
+        mc_e = [random.betavariate(float(min_gamma), float(max_gamma)) for _ in range(50000)]
+
+    else: 
+        mc_rf_alpha = [random.uniform(np.log10(float(min_alpha)), np.log10(float(max_alpha))) for _ in range(50000)]
+        mc_f = [random.normalvariate(float(media_beta), float(dp_beta)) for _ in range(50000)]
+        mc_e = [random.uniform(float(min_gamma), float(max_gamma)) for _ in range(50000)]
+
+    RF = np.mean(mc_rf_alpha)
+    F = np.mean(mc_f)
+    E = np.mean(mc_e)
+
+    return RF, F, E
+
+
+def run_simulation(df, FR, RF, F, E):
+    FR = float(FR)
+    print(type(df.iloc[0:, 0][0]))
+    NIP = (df.iloc[0:, 0].astype(float) * float(FR)) / (RF * F * E)
+    df['NIP'] = NIP
+
+    st.table(df.head())
+
+    csv = convert_df(df)
+    st.download_button(label="Download data as CSV",
+                       data=csv,
+                       file_name='table.csv',
+                       mime='text/csv')
 
 st.set_page_config(page_title="pySewage")
-
-IMAGE_SUPP = Image.open('logos.png')
-
 latext = r''' 
 $$ 
 NIP = \frac{VGC\cdot FR}{\alpha\cdot\beta\cdot\gamma}
 $$ 
 '''
 
-#st.sidebar.markdown('# Navigation:')
-#nav = st.sidebar.radio('Go to:', ['HOME', 'Simulation', 'Citation', 'Contact'])
-        
 with st.sidebar:
-    nav = option_menu('Navegation:', ['HOME', 'Simulation', 'Citation', 'Contact'], 
-    icons=['house', 'water', 'journal-check',  'chat-left-text-fill'], menu_icon="cast", default_index=0)
-        
+    nav = option_menu('Navigation:', ['HOME', 'Simulation SARS-CoV-2', 'Simulation Arboviruses',  'Citation', 'Contact'], 
+                      icons=['house', 'water', 'bug', 'journal-check',  'chat-left-text-fill'], menu_icon="cast", default_index=0)
+
 gettext = Texts()
 text1_1 = gettext.text1_1()
 text4 = gettext.text4()
@@ -32,136 +204,77 @@ st.sidebar.info('{}'.format(text4))
 
 filecsv = pd.read_csv("template.csv")
 
-@st.cache
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
-
 def local_css(file_name):
     with open(file_name) as f:
         return st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 if nav == 'HOME':
     gettext = Texts()
-    
     text1 = gettext.text1()
     st.markdown('# pySewage')
     st.markdown('{}'.format(text1), unsafe_allow_html=True)
-    
     st.write(latext)
-    
     text2 = gettext.text2()
     st.markdown('{}'.format(text2), unsafe_allow_html=True)
-    
     csv = convert_df(filecsv)
     st.download_button(label="Download template csv",
-                                   data=csv,
-                                   file_name='template.csv',
-                                   mime='text/csv')
-    
+                       data=csv,
+                       file_name='template.csv',
+                       mime='text/csv')
     text3 = gettext.text3()
     st.markdown('{}'.format(text3), unsafe_allow_html=True)
-    
+    IMAGE_SUPP = Image.open('logos.png')
     st.image(IMAGE_SUPP, use_column_width=True)
 
-
-if nav == 'Simulation':
-    st.header(r'Monte Carlo Simulation for infection prevalence estimation')
-
-    file = st.file_uploader("Pick a file")
-
-    if file:    
-        df = pd.read_csv(file)
-
-        FR = st.text_input('Type your waterwaste flow rate data (L/s)', )
-
+if nav == 'Simulation SARS-CoV-2':
+    st.header(r'Monte Carlo Simulation for SARS-CoV-2 infection prevalence estimation.')
+    session_id = "session_1"
+    file1 = st.file_uploader("Pick a file", key=session_id)
+    if file1:    
+        df = pd.read_csv(file1)  # Não há necessidade de pular a primeira linha
+        FR = st.text_input('Type your waterwaste flow rate data (L/day)', )
         k_auto_manual = st.radio("Choose your parameters (alpha, beta and gamma)", ('Automatic', 'Manual'))
-
         if k_auto_manual == 'Automatic':
-
             generate = st.button("Generate")
             if generate:
-
-                VCG = df['Genomic Copies/L']
-                FR = 1866
-
-                mc_rf_aut = []; mc_rf_uae = []; mc_rf_abc = []; mc_f = []; mc_e = []
-                for i in range(1,50000+1):
-                    #mc_rf_aut.append(random.normalvariate(2.11, 0.25))
-                    mc_rf_abc.append(random.uniform(np.log10(6.3E5), np.log10(1.3E8)))
-                    mc_f.append(random.normalvariate(149, 95))
-                    mc_e.append(random.uniform(0.29, 0.55))
-
-                RF = np.mean(mc_rf_abc); F = np.mean(mc_f); E = np.mean(mc_e);  
-                df['NIP'] = (df['Genomic Copies/L'] * FR) / (RF * F * E)
-
-                st.table(df.head())
-
-                csv = convert_df(df)
-                st.download_button(label="Download data as CSV",
-                                   data=csv,
-                                   file_name='table.csv',
-                                   mime='text/csv')
-
-
-        if k_auto_manual == 'Manual':
-
-            VCG = df['Genomic Copies/L'].values.tolist()
-
-            col1, col2 = st.columns(2)
-
-            with col1:
-                min_alpha = st.text_input('Add the minimum alpha parameter value', )
-
-            with col2:
-                max_alpha = st.text_input('Add the maximum value of the alpha parameter', )
-
-            col3, col4 = st.columns(2)
-
-            with col3:
-                media_beta = st.text_input('Add the average of the beta parameter', )
-
-            with col4:
-                dp_beta = st.text_input('Add the standard deviation of the beta parameter', )
-
-            col5, col6 = st.columns(2)
-
-            with col5:
-                min_gamma = st.text_input('Add the minimum value of the gamma parameter', )
-
-            with col6:
-                max_gamma = st.text_input('Add the maximum value of the gamma parameter', )
-
+                #VCG = df.iloc[1:, 0].tolist()
+                RF, F, E = generate_parameters_auto()
+                run_simulation(df, FR, RF, F, E)
+        elif k_auto_manual == 'Manual':
+            #VCG_column = df.iloc[1:, 0].tolist()
+            min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma = parameter_inputs()
             generate = st.button("Generate")
             if generate:
-            
-                mc_rf_alpha = []; mc_f = []; mc_e = []
-                for i in range(1,50000+1):
-                    mc_rf_alpha.append(random.uniform(math.log10(float(min_alpha)), math.log10(float(max_alpha))))
-                    #mc_rf_alpha.append(random.uniform(np.log10(6.3E5), np.log10(1.3E8)))
-                    mc_f.append(random.normalvariate(float(media_beta), float(dp_beta)))
-                    mc_e.append(random.uniform(float(min_gamma), float(max_gamma)))
+                RF, F, E = generate_parameters_manual(min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma)
+                run_simulation(df, FR, RF, F, E)
 
-                RF = np.mean(mc_rf_alpha, dtype='float'); F = np.mean(mc_f, dtype='float'); E = np.mean(mc_e, dtype='float');  
-                
-                
-                NIP = [ ((x*float(FR)) / (RF * F  * E)) for x in VCG ]
-
-                df['NIP'] = NIP
-
-                st.table(df.head())
-
-                csv = convert_df(df)
-                st.download_button(label="Download data as CSV",
-                                   data=csv,
-                                   file_name='table.csv',
-                                   mime='text/csv')
+elif nav == 'Simulation Arboviruses':
+    st.header(r'Monte Carlo Simulation for arboviruses infection prevalence estimation.')
+    session_id = "session_2"
+    file2 = st.file_uploader("Pick a file", key=session_id)
+    if file2:    
+        df = pd.read_csv(file2)
+        FR = st.text_input('Type your waterwaste flow rate data (L/person/day)', )
+        arbovirus = st.selectbox("Choose the arbovirus:", ['DENV', 'ZIKV', 'CHIKV'])
+        k_auto_manual = st.radio("Choose your parameters (alpha, beta and gamma)", ('Automatic', 'Manual'))
+        if k_auto_manual == 'Automatic':
+            generate = st.button("Generate")
+            if generate:
+                #VCG = df.iloc[1:, 0].tolist()
+                RF, F, E = generate_parameters_auto(arbovirus)
+                run_simulation(df, FR, RF, F, E)
+        elif k_auto_manual == 'Manual':
+            #VCG = df.iloc[1:, 0].tolist()
+            min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma = parameter_inputs(arbovirus)
+            generate = st.button("Generate")
+            if generate:
+                RF, F, E = generate_parameters_manual(min_alpha, max_alpha, media_beta, dp_beta, min_gamma, max_gamma, arbovirus)
+                run_simulation(df, FR, RF, F, E)
 
 if nav == 'Citation':
     st.markdown('{}'.format(text1_1), unsafe_allow_html=True)
 
-
 if nav == 'Contact':
-    #st.header(":mailbox: Entre em contato comigo!!")
     st.header("Contact me!!")
     contact_form = """
     <form action="https://formsubmit.co/adrianoarvs@gmail.com" method="POST">
@@ -175,6 +288,3 @@ if nav == 'Contact':
 
     st.markdown(contact_form, unsafe_allow_html=True)
     local_css("style/style.css")
-
-
-
--- texts.py
+++ texts.py
@@ -4,8 +4,7 @@ class Texts:
 
     def text1(self):
         TEXT1 = """
-        <p style='text-align: justify; color: black;'> This application consists of the prediction of people infected by SARS-CoV-2 from viral load present in wastewater samples. 
-        The user will be able to simulate an automatic or manual prediction according to the equation: </p>
+        <p style='text-align: justify; color: black;'> This application is engineered to estimate the count of individuals infected with various viral pathogens, including SARS-CoV-2, DENV, ZIKV, and CHIKV, by analyzing the viral load detected in wastewater samples. It provides users with the flexibility to generate predictions either automatically or manually, following a given equation: </p>
         """
 
         return TEXT1
@@ -14,10 +13,11 @@ class Texts:
         TEXT1_1 = """
         <body style='text-align: justify; color: black;'>
         <h1 style='text-align: justify; color: black;'>pySewage website</h1>
-        <p> pySewage - a program that permits of the prediction of people infected by SARS-CoV-2.
+        <p> pySewage - a comprehensive software tool that facilitates the prediction of individuals infected by various viral pathogens, including DENV, ZIKV, CHIKV, and SARS-CoV-2.
         <h2 style='text-align: justify; color: black;'>Citation</h2>
         Please cite the following reference when publishing results obtained with pySewage:<br>
-        (i) DE SOUSA, Adriano Roberto Vieira, et al. “pySewage”: a hybrid approach to predict the number of SARS-CoV-2-infected people from wastewater in Brazil. Environmental Science and Pollution Research, 2022, 1-10, <a href="https://doi.org/10.1007/s11356-022-20609-z"target="_blank">https://doi.org/10.1007/s11356-022-20609-z</a>
+        (i) DE SOUSA, Adriano Roberto Vieira, et al. “pySewage”: a hybrid approach to predict the number of SARS-CoV-2-infected people from wastewater in Brazil. Environmental Science and Pollution Research, 2022, 1-10, <a href="https://doi.org/10.1007/s11356-022-20609-z"target="_blank">https://doi.org/10.1007/s11356-022-20609-z</a>;<br>
+        (ii) DE SOUSA, Adriano Roberto Vieira, et al. Arboviruses Wastewater Surveillance in Brazil’s Midwest Region: Development of a Predictive Web Application. Submitted. 
         </p>
         </body>
         """
@@ -29,10 +29,10 @@ class Texts:
         where:<br>                     
         NIP: number of infected people;<br> 
         VGC: Viral genomic copies detected in wastewater (viral gene load/L);<br>
-        FR: Wastewater flow rate (L/day);<br>
+        FR: Wastewater flow rate (L/person/day);<br>
         α: Viral load in the stool (viral gene copies/g stool);<br>
-        β: Daily production of stool per capita (g stool/capita.day);<br>
-        γ: % of COVID-19 patients who shed virus in their stool.<br>
+        β: Daily production of stool (SARS-CoV-2) per capita (g stool/capita.day) or urine (arboviruses) per capita (mL urine/capita.day) ;<br>
+        γ: % of infected patients who shed virus in their stool (SARS-CoV-2) or urine (arboviruses).<br>
         
         <p style='text-align: justify; color: black;'> To start the simulation, the user must add a csv file (download template below) 
         with the viral load detected in the wastewater (VGC) and the flow rate (FR). 
@@ -40,7 +40,10 @@ class Texts:
         standard parameters of alpha, beta, and gamma. For manual simulation, 
         the user must add the values of alpha, beta, and gamma parameters. 
         To reduce uncertainties regarding the equation's variables, 
-        the web application employs Monte Carlo simulation in alpha, beta and gamma variables.</p>
+        the web application employs Monte Carlo simulation in alpha, beta and gamma variables.<br><br>
+        Note that for SARS-CoV-2 simulation, the data should be in genomic copies per liter <b>(genomic copies/L)</b>. 
+        For arbovirus simulation, however, the data should be in genomic copies per log10 per liter <b>(genomic copies/log10/L)</b>.
+        </p>
         """
 
         return TEXT2
